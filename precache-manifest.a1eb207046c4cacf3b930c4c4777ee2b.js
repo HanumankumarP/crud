2a2b471c1e47db84a2b05d19d9319882d93cd7c9
fournotfourerror <fournotfourerror@gmail.com>
2019-11-30 10:23:56 +0530

    Updates

---
+++ precache-manifest.a1eb207046c4cacf3b930c4c4777ee2b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d5c178b4d47bb76f65659daa2687d205",
+    "url": "/crud/index.html"
+  },
+  {
+    "revision": "c0c1e97098aa7cd01cac",
+    "url": "/crud/static/css/main.5c9fd624.chunk.css"
+  },
+  {
+    "revision": "d78065286ff9bc99211d",
+    "url": "/crud/static/js/2.b8a83d7c.chunk.js"
+  },
+  {
+    "revision": "c0c1e97098aa7cd01cac",
+    "url": "/crud/static/js/main.833797b2.chunk.js"
+  },
+  {
+    "revision": "5a5fb1e13f491c10a54d",
+    "url": "/crud/static/js/runtime-main.aa76378b.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/crud/static/media/logo.25bf045c.svg"
+  }
+]);
